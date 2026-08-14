c1d9cf477ced7e369bb05ad9e126841efce6d6e8
Jui23 <juitawde23@gmail.com>
2026-08-14 13:28:49 +0530

    modified

--- server.js
+++ server.js
@@ -30,7 +30,10 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      maxAge: 1000 * 60 * 60 * 24
+      maxAge: 1000 * 60 * 60 * 24,
+      httpOnly: true,
+      secure: true,
+      sameSite: "none"
     }
   })
 );
